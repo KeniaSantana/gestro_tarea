251d9a962be9ce0e6dc2162d8340355936a4a089
Santana Ruiz Kenia Alejandra <23308060610371@cetis61.edu.mx>
2026-05-06 19:45:33 -0600

    Fix typos in page titles and route handling

--- src/main.py
+++ src/main.py
@@ -3,59 +3,77 @@ from controllers.UserController import AuthController
 from controllers.TareasController import TareaController
 from views.LoginView import LoginView
 from views.dashboard import DashboardView
+from views.registro import RegistroView
+
 
 def start(page: ft.Page):
-    page.title = "Sistema de inicio de sesion"
+
+    page.title = "Sistema de inicio de sesión"
     page.window_width = 450
     page.window_height = 700
-    
+
     auth_ctrl = AuthController()
     task_ctrl = TareaController()
-    
+
     def route_change(e):
+
         page.views.clear()
 
+
         if page.route == "/":
-            page.views.append(LoginView(page, auth_ctrl))
-        
+
+            page.views.append(
+                LoginView(page, auth_ctrl)
+            )
+
+
         elif page.route == "/dashboard":
-            page.views.append(DashboardView(page, task_ctrl))
-        
-        elif page.route == "/registro":
+
+            page.views.append(
+                DashboardView(page, task_ctrl)
+            )
+
+        elif page.route == "/registrarse":
+
+            page.views.append(
+                RegistroView(page, auth_ctrl)
+            )
+
+
+        else:
+
             page.views.append(
                 ft.View(
-                    route="/registro",  
+                    route="/",
                     controls=[
-                        ft.Text("Pantalla de registro"),
-                        ft.ElevatedButton("Volver", on_click=lambda _: page.go("/"))
+                        ft.Text("Ruta no encontrada")
                     ]
                 )
             )
-        
-        else:
-            page.views.append(
-                ft.View(
-                    route="/", 
-                    controls=[ft.Text("Ruta no encontrada")]
-                )
-            )
-        
+
         page.update()
-        
+
     def view_pop(e):
+
         if len(page.views) > 1:
+
             page.views.pop()
+
             top_view = page.views[-1]
+
             page.go(top_view.route)
-    
+
     page.on_route_change = route_change
     page.on_view_pop = view_pop
+
+
 
     print("Iniciando navegacion....")
     if page.route=="/":
         route_change(None)
     else:
         page.go("/")
+
 
 def main():
     ft.app(target=start)
