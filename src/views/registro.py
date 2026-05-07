0e84b82a3c0d5d3dc8b3a58df65b774b12e1ce38
Santana Ruiz Kenia Alejandra <23308060610371@cetis61.edu.mx>
2026-05-06 19:44:59 -0600

    Refactor registration logic and improve UI elements

--- src/views/registro.py
+++ src/views/registro.py
@@ -36,53 +36,86 @@ def RegistroView(page: ft.Page, auth_controller):
 
     def registrarse_click(e):
 
-        # Validar campos vacíos
-        if (
-            not nombre_input.value or
-            not apellido_input.value or
-            not email_input.value or
-            not telefono_input.value or
-            not pass_input.value
-        ):
+        print("BOTON PRESIONADO")
 
-            page.snack_bar = ft.SnackBar(
-                ft.Text("Por favor, llene todos los campos")
+        try:
+
+
+            if (
+                not nombre_input.value or
+                not apellido_input.value or
+                not email_input.value or
+                not telefono_input.value or
+                not pass_input.value
+            ):
+
+                print("FALTAN CAMPOS")
+
+                page.snack_bar = ft.SnackBar(
+                    ft.Text("Por favor, llene todos los campos")
+                )
+
+                page.snack_bar.open = True
+                page.update()
+
+                return
+
+            nombre = nombre_input.value
+            apellido = apellido_input.value
+            email = email_input.value
+            telefono = telefono_input.value
+            password = pass_input.value
+
+            print(
+                nombre,
+                apellido,
+                email,
+                telefono
             )
 
-            page.snack_bar.open = True
-            page.update()
-            return
 
-        nombre = nombre_input.value
-        apellido = apellido_input.value
-        email = email_input.value
-        telefono = telefono_input.value
-        password = pass_input.value
-
-        # Registrar usuario
-        success, msg = auth_controller.registrar_usuario(
-            nombre,
-            apellido,
-            email,
-            password,
-            telefono
-        )
-
-        if success:
-
-            page.snack_bar = ft.SnackBar(
-                ft.Text("Usuario registrado correctamente")
+            success, msg = auth_controller.registrar_usuario(
+                nombre,
+                apellido,
+                email,
+                password,
+                telefono
             )
 
-            page.snack_bar.open = True
-            page.update()
+            print("RESULTADO:", success, msg)
 
-            page.go("/")
 
-        else:
+            if success:
+
+                print("USUARIO REGISTRADO")
+
+                page.snack_bar = ft.SnackBar(
+                    ft.Text(msg)
+                )
+
+                page.snack_bar.open = True
+                page.update()
+
+
+                page.go("/")
+
+            else:
+
+                print("NO SE REGISTRO")
+
+                page.snack_bar = ft.SnackBar(
+                    ft.Text(msg)
+                )
+
+                page.snack_bar.open = True
+                page.update()
+
+        except Exception as ex:
+
+            print("ERROR REAL:", ex)
 
             page.snack_bar = ft.SnackBar(
-                ft.Text(msg)
+                ft.Text(f"Error: {ex}")
             )
 
             page.snack_bar.open = True
@@ -92,26 +125,16 @@ def RegistroView(page: ft.Page, auth_controller):
         "Crear cuenta",
         on_click=registrarse_click,
         width=350,
-        bgcolor="cyan",
-        color="black",
-        icon=ft.Icon(
-            ft.Icons.CHECK,
-            color=ft.Colors.WHITE,
-            size=25
-        )
+        bgcolor="#F7ADC4",
+        color="black"
     )
 
     volver_button = ft.ElevatedButton(
         "¿Ya tienes una cuenta?",
         on_click=lambda _: page.go("/"),
         width=350,
-        bgcolor="green",
-        color="black",
-        icon=ft.Icon(
-            ft.Icons.LOGIN,
-            color=ft.Colors.WHITE,
-            size=25
-        )
+        bgcolor="#CAA1F8",
+        color="black"
     )
 
     pass_input.on_submit = registrarse_click
@@ -123,17 +146,19 @@ def RegistroView(page: ft.Page, auth_controller):
         horizontal_alignment=ft.CrossAxisAlignment.CENTER,
 
         appbar=ft.AppBar(
-            title=ft.Text("REGISTRO :)"),
-            bgcolor="cyan",
-            color="white"
+            title=ft.Text("Registro"),
+            bgcolor="#CAA1F8"
         ),
 
         controls=[
+
             ft.Column(
+
                 [
+
                     ft.Text(
-                        "Regístrate",
-                        size=24,
+                        "Crear cuenta",
+                        size=25,
                         weight="bold"
                     ),
 
@@ -144,12 +169,14 @@ def RegistroView(page: ft.Page, auth_controller):
                     pass_input,
 
                     registrarse_button,
-                    volver_button,
+                    volver_button
+
                 ],
 
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                tight=True,
                 spacing=20
+
             )
+
         ]
     )
